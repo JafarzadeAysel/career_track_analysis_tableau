a202e50a93faa692c0cdec1e78b0fbce395a9b13
AyselJafarzade <jafarzadeaysel8@gmail.com>
2025-02-22 21:06:46 +0400

    Dashboard finished

--- script.sql
+++ script.sql
@@ -9,7 +9,7 @@ WHEN datediff(date_completed,a.date_enrolled) between 8 and 30 then "8 to 30 day
 WHEN datediff(date_completed,a.date_enrolled) between 31 and 60 then "31 to 60 days"
 WHEN datediff(date_completed,a.date_enrolled) between 61 and 90 then "61 to 90 days"
 WHEN datediff(date_completed,a.date_enrolled) between 91 and 365 then "91 to 365 days"
-WHEN datediff(date_completed,a.date_enrolled) >366 then "1 to 7 days"
+WHEN datediff(date_completed,a.date_enrolled) >366 then "366+"
 END AS completion_bucket
 FROM career_track_student_enrollments a
 Left JOIN career_track_info b
